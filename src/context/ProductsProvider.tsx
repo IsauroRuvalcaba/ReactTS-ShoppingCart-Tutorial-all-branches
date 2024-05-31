8cb1cefb023b09c78b010a9f888f1fa41c58f65d
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2024-05-30 21:16:50 -0700

    added a json server and fetched the data from the data folder on port 3500 and then used setProducts to added it to the useState used for products. If rather use the static file, then uncomment the const initState and comment the useEffect used to load the useState.

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, createContext, useState } from "react";
+import { ReactElement, createContext, useEffect, useState } from "react";
 
 export type ProductType = {
   sku: string;
@@ -6,23 +6,25 @@ export type ProductType = {
   price: number;
 };
 
-const initState: ProductType[] = [
-  {
-    sku: "item0001",
-    name: "Widget",
-    price: 9.99,
-  },
-  {
-    sku: "item0002",
-    name: "Premium Widget",
-    price: 19.99,
-  },
-  {
-    sku: "item0003",
-    name: "Deluxe Widget",
-    price: 29.99,
-  },
-];
+const initState: ProductType[] = [];
+
+// const initState: ProductType[] = [
+//   {
+//     sku: "item0001",
+//     name: "Widget",
+//     price: 9.99,
+//   },
+//   {
+//     sku: "item0002",
+//     name: "Premium Widget",
+//     price: 19.99,
+//   },
+//   {
+//     sku: "item0003",
+//     name: "Deluxe Widget",
+//     price: 29.99,
+//   },
+// ];
 
 //this is needed when we build the Context which requires a type
 export type UseProductsContextType = { products: ProductType[] };
@@ -37,6 +39,22 @@ type ChildrenType = { children?: ReactElement | ReactElement[] };
 export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
   //sometimes Reducer is used here instead of useState if state is bigger
   const [products, setProducts] = useState<ProductType[]>(initState);
+
+  //if not using this, uncomment the initialstate above. Running json server
+  useEffect(() => {
+    const fetchProducts = async (): Promise<ProductType[]> => {
+      const data = await fetch("http://localhost:3500/products")
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => {
+          if (err instanceof Error) console.log(err.message);
+        });
+      return data;
+    };
+
+    fetchProducts().then((products) => setProducts(products));
+  }, []);
 
   return (
     <ProductsContext.Provider value={{ products }}>
