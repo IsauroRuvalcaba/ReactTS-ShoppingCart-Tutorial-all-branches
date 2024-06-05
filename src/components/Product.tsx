b0ec1900348993ccd373e47100ab97325d77bb8b
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2024-06-04 21:26:02 -0700

    to import images when using Vite, have to use new URL and thats what we did here

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -15,6 +15,12 @@ const Product = ({
   REDUCER_ACTIONS,
   inCart,
 }: ProductType): ReactElement => {
+  // const img: string = require('../images/${product.sku}.jpg') //this dosent work with vite
+
+  const img: string = new URL(`../images/${product.sku}.jpg`, import.meta.url)
+    .href;
+  console.log(img);
+
   return <div>Product</div>;
 };
 
